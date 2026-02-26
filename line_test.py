5afdf6123ab2c838d4de10901a4619dca680decd
yagi8386 <yagiko1224@gmail.com>
2026-02-27 00:47:04 +0900

    Refactor line_test.py for improved functionality

--- line_test.py
+++ line_test.py
@@ -13,6 +13,8 @@ URLS = {
     "横須賀": "https://as.its-kenpo.or.jp/apply/empty_new?s=PUlETnpJVFBrbG1KbFZuYzAxVFp5Vkhkd0YyWWZWR2JuOTJiblpTWjFKSGQ5a0hkdzFXWg%3D%3D",
     "河口湖": "https://as.its-kenpo.or.jp/apply/empty_new?s=PVlETzFFVFBrbG1KbFZuYzAxVFp5Vkhkd0YyWWZWR2JuOTJiblpTWjFKSGQ5a0hkdzFXWg%3D%3D",
     "館山": "https://as.its-kenpo.or.jp/apply/empty_new?s=PT1RTTJjVFBrbG1KbFZuYzAxVFp5Vkhkd0YyWWZWR2JuOTJiblpTWjFKSGQ5a0hkdzFXWg%3D%3D",
+    "ラビスタ熱海": "https://as.its-kenpo.or.jp/apply/empty_new?s=PUlUTzFJVFBrbG1KbFZuYzAxVFp5Vkhkd0YyWWZWR2JuOTJiblpTWjFKSGQ5a0hkdzFXWg%3D%3D",
+    "鬼怒川": "https://as.its-kenpo.or.jp/apply/empty_new?s=PU1UTzFJVFBrbG1KbFZuYzAxVFp5Vkhkd0YyWWZWR2JuOTJiblpTWjFKSGQ5a0hkdzFXWg%3D%3D",
 }
 
 # ====== 空き判定キーワード ======
@@ -122,6 +124,7 @@ if not has_error:
     save_last_dates(current_dates)
 else:
     print("エラーがあったため状態保存をスキップ")
+
 
 
 
